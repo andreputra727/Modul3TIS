97797042576a241c6771c9df59f68bc4946ff76b
andreputra727 <andreputrasrg946@gmail.com>
2026-03-13 16:28:45 +0700

    Create ValidateBookYear middleware
    Add middleware to validate book year in requests

---
+++ ValidateBookYear.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ValidateBookYear
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $year = $request->year;
+        if ($year) {
+            if ($year < 1900 || $year > date('Y')) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Invalid book year'
+                ], 400);
+            }
+        }
+        return $next($request);
+    }
+}
